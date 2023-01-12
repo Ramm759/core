d25a87065022ceef5f51c96f43591e373cc6a7ba
Ramm759 <Prozac759>
2023-01-12 20:41:42 +0100

    Exercice 31 : DVDStore : Exploiter Spring Data JDBC

--- src/main/java/com/mycompany/dvdstore/entity/Movie.java
+++ src/main/java/com/mycompany/dvdstore/entity/Movie.java
@@ -1,11 +1,37 @@
 package com.mycompany.dvdstore.entity;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Movie {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String genre;
     private String description;
+
+    public Movie(Long id, String title, String genre, String description) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.description = description;
+    }
+
+    public Movie(Long id, String title, String genre) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public Movie() {
+
+    }
 
     public String getTitle() {
         return title;
